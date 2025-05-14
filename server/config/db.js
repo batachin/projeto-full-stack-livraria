09233f67dae0d014011872c0f4be6be06131e3e4
batachin <nadagatodamafia@gmail.com>
2025-05-13 21:57:05 -0300

    commit 4

--- server/config/db.js
+++ server/config/db.js
@@ -4,18 +4,12 @@ require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
 
-const config = require('config');
-//const db = config.get('mongoURI');
-uri = process.env.MONGODB_URI;
-
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
         await mongoose.connect(uri, {
             useNewUrlParser: true,
-
         });
-
 
         console.log('MongoDB está conectado...');
     } catch (err) {
